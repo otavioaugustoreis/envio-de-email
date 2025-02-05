7a6732b16ac5c0f2f81d04a113fb3b9ff8cc17cc
Otavio Augusto Reis Almeida <otavio.almeida@pwi.com.br>
2025-02-05 15:28:35 -0300

    feat: Configurando o EnvioEmail

---
+++ EnvioDeEmail/Controllers/EmailController.cs
@@ -0,0 +1,33 @@
+﻿using FluentEmail.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnvioDeEmail.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private IFluentEmail _fluentEmail;
+
+        public EmailController(IFluentEmail fluentEmail)
+        {
+            _fluentEmail = fluentEmail;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EnviarEmail([FromBody] string destinatario)
+        {
+
+            var enviar = await _fluentEmail.To(destinatario)
+                               .Body("The body")
+                               .SendAsync();
+            if (!enviar.Successful)
+            {
+                return BadRequest($"Ocorreu um problema ao enviar e-mail para : {destinatario}");
+            }
+
+            return Ok("E-mail enviado com sucesso");
+        }
+    }
+}
--- EnvioDeEmail/Program.cs
+++ EnvioDeEmail/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFluentEmail("oaugusto265@gmail.com");
 
 var app = builder.Build();
 
